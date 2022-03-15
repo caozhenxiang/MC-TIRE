5f80265e348e95c99b59ea5b2a233b70e8dabcb6
Zhenxiang Cao <caozhenxiang2017@gmail.com>
2022-03-15 08:18:10 +0100

    minor

--- functions/setup.py
+++ functions/setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup, find_packages
+import os
+
+path = os.path.abspath(os.path.join(os.getcwd(), ".."))
+with open(os.path.join(path, "README.md"), "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name='MC-TIRE',
     version='1.0',
+    author="Zhenxiang Cao",
+    author_email="zhenxiang.cao@esat.kuleuven.be",
+    url="https://github.com/caozhenxiang/MC-TIRE",
+    long_description=long_description,
     packages=find_packages(),
     include_package_data=True,
     package_data={},
